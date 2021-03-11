98f7cc33226685e2ff8987c664e0a0088c00c89c
hihumikan <musecharo@gmail.com>
2021-03-11 18:32:33 +0900

    nanmonai

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
     <div class="flex justify-center text-5xl mt-28">
-      <p>qqey.net</p>
+        <p>{{ message }}</p>
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      message: "qqey.net",
+    };
+  },
+};
 </script>
 
 <style></style>
