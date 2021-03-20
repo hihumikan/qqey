75ecbd88ecade7e6098c10b3c4d7e1dde30585a6
hihumikan <musecharo@gmail.com>
2021-03-21 06:18:59 +0900

    レスポンシブかなり良くした

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,28 +2,40 @@
   <v-app>
     <v-container>
       <v-row class="lighten-2" style="height: 400px">
-        <v-col cols="12" sm="8" md="6" lg="4" xl="3">
-          <img
-            src="https://avatars.githubusercontent.com/u/26848713?s=460&u=b5c630322f32d86c35ec2a62469f36502755d83c&v=4"
-            alt="プロフィール画像"
-            title="プロフィール画像"
-            class="rounded-full h-52 w-52 md:h-72 md:w-72 m-6"
-          />
+        <v-col cols="12" sm="10" md="4" lg="6" xl="1">
+          <center>
+            <img
+              src="https://avatars.githubusercontent.com/u/26848713?s=460&u=b5c630322f32d86c35ec2a62469f36502755d83c&v=4"
+              alt="プロフィール画像"
+              title="プロフィール画像"
+              class="rounded-full h-52 w-52 md:h-72 md:w-72"
+            />
+          </center>
         </v-col>
-        <v-col  cols="12" sm="10" md="6" lg="4" xl="3">
-          <v-card class="mt-24" max-width="500" outlined>
-            <v-list-item three-line>
-              <v-list-item-content class="flex">
-                <p class="text-4xl text-center font-semibold">hihumikan</p>
 
-                <p class="mt-8 text-lg">
-                  雑食系で赴くままに中途半端に色々手を出す。
-                </p>
-              </v-list-item-content>
-            </v-list-item>
-          </v-card>
+        <v-col cols="12" sm="10" md="8" lg="6" xl="1">
+          <center>
+            <v-card class="mt-24" max-width="500" outlined>
+              <v-list-item three-line>
+                <v-list-item-content>
+                  <p class="text-4xl text-center font-semibold">hihumikan</p>
+                  <center>
+                    <p class="mt-8 text-lg font-semibold">
+                      雑食系で赴くままに中途半端に色々手を出す。
+                    </p>
+                  </center>
+                  <p class="text-lg">
+                    フロントエンドとかバックエンド、インフラのあれこれやってます。
+                  </p>
+                  <p class="text-lg mt-3">
+                    ご連絡はTwitterかTelegramでお願いします。
+                  </p>
+                </v-list-item-content>
+              </v-list-item>
+            </v-card>
+          </center>
         </v-col>
-        <v-col  cols="12">
+        <v-col cols="12" sm="8" md="12" lg="10" xl="1">
           <font-awesome-icon
             class="text-6xl md:text-7xl ml-20 md:ml-40 mt-10 cursor-pointer TwitterCSS"
             :icon="['fab', 'twitter']"
@@ -69,7 +81,7 @@ export default {
   transition: all 0.3s ease;
 }
 .TwitterCSS:hover {
-  color: #1DA1F2;
+  color: #1da1f2;
 }
 .GitHubCSS {
   -webkit-transition: all 0.3s ease;
@@ -88,9 +100,15 @@ export default {
   transition: all 0.3s ease;
 }
 .TelegramCSS:hover {
-  color: #0088CC;
+  color: #0088cc;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 3s ease-out;
 }
 
-
-
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <div class="flex justify-center text-5xl mt-28">
+      <transition appear>
         <p>{{ message }}</p>
+      </transition>
     </div>
   </div>
 </template>
@@ -11,10 +13,22 @@ export default {
   data() {
     return {
       message: "qqey.net",
+      show: true,
     };
   },
+  methods: {},
 };
 </script>
 
 <style>
+.v-enter-active,
+.v-leave-active {
+  transition: 1.6s ease-out;
+}
+
+.v-enter,
+.v-leave-to {
+  opacity: 0;
+  transform: translateY(-30px);
+}
 </style>
