0225b0fe91a6b71b913f31386cf5a4bb6ff04367
hihumikan <musecharo@gmail.com>
2021-03-21 03:45:05 +0900

    navbar 修正

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
           <span class="font-semibold text-2xl tracking-tight">qqey.net</span>
         </router-link>
       </div>
-      <ul class="md:border-none flex justify-end m-0 w-full md:w-auto">
-        <li class="border-t md:border-none">
+      <ul class="flex justify-end md:w-auto">
+        <li class="md:border-none">
           <router-link to="/">
             <a
               class="block md:inline-block px-4 py-3 hover:text-yellow-400 no-underline"
@@ -16,19 +16,11 @@
           </router-link>
         </li>
 
-        <li class="border-t md:border-none">
+        <li class="md:border-none">
           <router-link to="/about">
             <a
               class="block md:inline-block px-4 py-3 hover:text-yellow-400 no-underline"
               >About</a
-            >
-          </router-link>
-        </li>
-        <li class="border-t md:border-none">
-          <router-link to="/contact">
-            <a
-              class="block md:inline-block px-4 py-3 hover:text-yellow-400 no-underline"
-              >Contact</a
             >
           </router-link>
         </li>
