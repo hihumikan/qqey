dbad759b23ec457e04fa672eda020c4d0fe12012
hihumikan <musecharo@gmail.com>
2021-03-21 03:12:33 +0900

    about change

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,26 +1,50 @@
 <template>
-  <div>
-    <div class="flex flex-nowrap">
-      <img
-        src="https://avatars.githubusercontent.com/u/26848713?s=460&u=b5c630322f32d86c35ec2a62469f36502755d83c&v=4"
-        alt="プロフィール画像"
-        title="プロフィール画像"
-        class="rounded-full h-52 w-52 md:h-72 md:w-72 m-6"
-      />
-      <div class="flex flex-col">
-        <font-awesome-icon
-          class="text-6xl md:text-7xl ml-20 md:ml-40 mt-10 cursor-pointer"
-          :icon="['fab', 'twitter']"
-          @click="twitter()"
-        />
-        <font-awesome-icon
-          class="text-6xl md:text-7xl ml-20 md:ml-40 mt-3 cursor-pointer"
-          :icon="['fab', 'github']"
-          @click="github()"
-        />
-      </div>
-    </div>
-  </div>
+  <v-app>
+    <v-container>
+      <v-row class="lighten-2" style="height: 400px">
+        <v-col>
+          <img
+            src="https://avatars.githubusercontent.com/u/26848713?s=460&u=b5c630322f32d86c35ec2a62469f36502755d83c&v=4"
+            alt="プロフィール画像"
+            title="プロフィール画像"
+            class="rounded-full h-52 w-52 md:h-72 md:w-72 m-6"
+          />
+        </v-col>
+        <v-col>
+          <v-card class="mt-24" max-width="500" outlined>
+            <v-list-item three-line>
+              <v-list-item-content>
+                <p class="text-4xl text-center font-semibold">hihumikan</p>
+
+                <p class="mt-8 text-lg">
+                  雑食系で赴くままに中途半端に色々手を出す。
+                </p>
+              </v-list-item-content>
+            </v-list-item>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <font-awesome-icon
+            class="text-6xl md:text-7xl ml-20 md:ml-40 mt-10 cursor-pointer TwitterCSS"
+            :icon="['fab', 'twitter']"
+            @click="twitter()"
+          />
+          <font-awesome-icon
+            class="text-6xl md:text-7xl ml-20 md:ml-40 mt-3 cursor-pointer GitHubCSS"
+            :icon="['fab', 'github']"
+            @click="github()"
+          />
+          <font-awesome-icon
+            class="text-6xl md:text-7xl ml-20 md:ml-40 mt-3 cursor-pointer TelegramCSS"
+            :icon="['fab', 'telegram']"
+            @click="telegram()"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
@@ -32,8 +56,43 @@ export default {
     github: function () {
       window.open("https://github.com/hihumikan");
     },
+    telegram: function () {
+      window.open("https://t.me/yata_touka");
+    },
   },
 };
 </script>
 
-<style></style>
+<style>
+.TwitterCSS {
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+}
+.TwitterCSS:hover {
+  color: #1DA1F2;
+}
+.GitHubCSS {
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+}
+.GitHubCSS:hover {
+  color: gray;
+}
+
+.TelegramCSS {
+  -webkit-transition: all 0.3s ease;
+  -moz-transition: all 0.3s ease;
+  -o-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+}
+.TelegramCSS:hover {
+  color: #0088CC;
+}
+
+
+
+</style>
